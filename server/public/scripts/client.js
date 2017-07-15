d92f17e454cf913ec109864a1d88dc28d76bfe29
Peter Prentiss <peter.prentiss@gmail.com>
2017-07-15 12:38:46 -0500

    Add comment show button functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,22 +8,23 @@ myApp.controller('GalleryController', function($scope){
   // {url: ../images'..., description: ..., likes: 0, visible: true}
 
   // increments like counter
-  $scope.addLike = function(photo) {
-    console.log('liked');
+  $scope.addLike = function (photo) {
     photo.likes++;
   }
   // toggles visible property so photo and description switch visibility
-  $scope.toggle = function(photo) {
-    console.log('toggle');
+  $scope.toggle = function (photo) {
     photo.visible = !photo.visible;
     //increments view counter when switching back to photo view
     if (photo.visible) {
       photo.views++
     }
   }
-
-  $scope.addComment = function(photo, comment) {
-    console.log('comment added');
+  // Adds comment to photo's comment array
+  $scope.addComment = function (photo, comment) {
     photo.comments.push(comment);
+  }
+  // Toggles photo's showComments property when clicked
+  $scope.showComments = function (photo) {
+    photo.showComments = !photo.showComments;
   }
 });
