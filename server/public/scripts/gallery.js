4d0b202372d5345d6cdcc036207b26c8e9edb754
Peter Prentiss <peter.prentiss@gmail.com>
2017-07-15 19:29:00 -0500

    looking good

--- server/public/scripts/gallery.js
+++ server/public/scripts/gallery.js
@@ -4,7 +4,7 @@
 let photoGallery = [
   {
     url: '../images/isaac_smile2.jpg',
-    description: 'The first time I got to meet my son. He was 2 months old and when I held him he looked me in the eyes and smiled. This is my favorite picture of all time',
+    description: 'The first time I got to meet my son. He was 2 months old and when I held him he looked me in the eyes and smiled. This is my favorite picture of all time.',
     comments: [],
     likes: 0,
     views: 1,
@@ -12,7 +12,7 @@ let photoGallery = [
     showComments: false
   }, {
     url: '../images/Selfiewisaac2.jpg',
-    description: 'Me with my beautiful son',
+    description: 'Me with my beautiful son.',
     comments: [],
     likes: 0,
     views: 1,
@@ -20,7 +20,7 @@ let photoGallery = [
     showComments: false
   }, {
     url: '../images/isaac_midlaugh.JPG',
-    description: 'Caught this picture in the middle of a huge laugh. Best face ever',
+    description: 'Caught this picture in the middle of a huge laugh. Best face ever.',
     comments: [],
     likes: 0,
     views: 1,
@@ -28,7 +28,7 @@ let photoGallery = [
     showComments: false
   }, {
     url: '../images/isaac_sleeping2.jpg',
-    description: 'Isaac having a good dream',
+    description: 'Isaac having a good dream.',
     comments: [],
     likes: 0,
     views: 1,
@@ -36,7 +36,7 @@ let photoGallery = [
     showComments: false
   }, {
     url: '../images/isaac_giftwrap_pope2.jpg',
-    description: 'On Christmas, Isaac wearing his pope hat made out of gift wrap',
+    description: 'On Christmas, Isaac wearing his pope hat made out of gift wrap.',
     comments: [],
     likes: 0,
     views: 1,
@@ -44,7 +44,7 @@ let photoGallery = [
     showComments: false
   }, {
     url: '../images/isaac_chewfinger2.jpg',
-    description: 'Isaac loved to chew on my fingers',
+    description: 'Isaac loved to chew on my fingers.',
     comments: [],
     likes: 0,
     views: 1,
