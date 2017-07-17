a61c6ec640d99405d90d8319a2496b6d6b15d532
Peter Prentiss <peter.prentiss@gmail.com>
2017-07-16 22:48:18 -0500

    Clean up, add comments and whitespace, delete dupe pictures.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,31 +1,29 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('GalleryController', function($scope){
+
   // array of data with photos and descriptions.
-  $scope.photoBank = photoGallery;
+  $scope.photoBank = photoGallery; // refers to array defined in gallery.js
+
   // increments like counter
   $scope.addLike = function (photo) {
     photo.likes++;
   }
+
   // toggles visible property so photo and description switch visibility
   $scope.toggle = function (photo) {
     photo.visible = !photo.visible;
-    //increments view counter when switching back to photo view
+    // increments view counter when switching back to photo view
     if (photo.visible) {
       photo.views++
     }
   }
+
   // Adds comment to photo's comment array
   $scope.addComment = function (photo, comment) {
     photo.comments.push(comment);
-    $scope.comment = '';
   }
 
-  $scope.comment = '';
-
-  $scope.reset = function() {
-    $scope.comment = angular.copy($scope.comment);
-  }
   // Toggles photo's showComments property when clicked
   $scope.showComments = function (photo) {
     photo.showComments = !photo.showComments;
